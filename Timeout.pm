fc30b59ff4e0c9096b4c2ddcb80d1959c64d45b1
Serguei Trouchelle <stro@cpan.org>
2010-11-01 14:54:16 +0000

    Import of STRO/Prompt-Timeout-1.03 from CPAN.
    gitpan-cpan-distribution: Prompt-Timeout
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         STRO/Prompt-Timeout-1.03.tar.gz
    gitpan-cpan-author:       STRO
    gitpan-cpan-maturity:     released

--- Timeout.pm
+++ Timeout.pm
@@ -1,6 +1,6 @@
 # Prompt/Timeout.pm
 #
-# $Id: Timeout.pm 1 2009-08-31 14:00:42Z stro $
+# $Id: Timeout.pm 24 2010-11-01 14:47:00Z stro $
 #
 # Copyright (c) 2008, 2009 Serguei Trouchelle. All rights reserved.
 #
@@ -8,7 +8,9 @@
 # under the same terms as Perl itself.
 
 # History:
-#  1.02  2009/08/31 Fixed problem with hanging in Unix environment, thanks to leocharre@cpan.org for reporting
+#  1.03  2010/11/01 Fixed problem with non-working "click", thanks to bottomsc[.]missouri.edu for reporting (RT#62535)
+#                   Fixed potential problem with TAP output parsing
+#  1.02  2009/08/31 Fixed problem with hanging in Unix environment, thanks to leocharre[.]cpan.org for reporting
 #  1.01  2008/05/23 Documentation typo
 #                   Removed Term::ReadKey::ReadMode because it doesn't work on some terminals
 #  1.00  2008/05/22 Initial revision
@@ -37,7 +39,7 @@ require Exporter;
 our @EXPORT = qw(prompt);
 our @ISA = qw(Exporter);
 
-$Prompt::Timeout::VERSION = '1.02';
+$Prompt::Timeout::VERSION = '1.03';
 
 =head1 DESCRIPTION
 
@@ -100,10 +102,12 @@ sub prompt ($;$$$) {
         print $key;
         $end = time + $timeout;
       } else {
-        if ($end < time and not ($click and $ans ne '')) {
-          $ans = $def;
-          print "$def\n";
-          last;
+        unless ($click and $ans ne '') {
+          if ($end < time) {
+            $ans = $def;
+            print "$def\n";
+            last;
+          }
         }
       }
     }
@@ -116,11 +120,11 @@ sub prompt ($;$$$) {
 
 Serguei Trouchelle E<lt>F<stro@railways.dp.ua>E<gt>
 
-FindBin::Real uses partial code from ExtUtils::MakeMaker module.
+Prompt::Timeout uses partial code from ExtUtils::MakeMaker module.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2008, 2009 Serguei Trouchelle. All rights reserved.
+Copyright (c) 2008-2010 Serguei Trouchelle. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- t/t3.t
+++ t/t3.t
@@ -1,21 +1,22 @@
 #!/usr/bin/env perl -w
 
-# $Id: t3.t 1 2009-08-31 14:00:42Z stro $
+# $Id: t3.t 24 2010-11-01 14:47:00Z stro $
 
 use strict;
 use Test;
 
-BEGIN { plan tests => 3 }
+BEGIN { plan tests => 4 }
 
 use Prompt::Timeout;
 
 $|=1;
 
-print "# This test case's running time is 2 minutes. Use Ctrl-Break to break, Ctrl-C may not work.\n";
+print STDERR "# This test case's running time is 2 minutes. Use Ctrl-Break to break, Ctrl-C may not work. Don't press any keys during testing run.\n";
 
 ok(prompt('Press <Enter> or wait 1 second. Do not press any other key!', 'DEF', 1, 1), 'DEF');
+ok(prompt('Press <Enter> or wait 1 second. Do not press any other key!', 'DEF', 1), 'DEF');
 ok(prompt('Press <Enter> or wait 60 seconds. ', 'DEF'), 'DEF'); # 60 seconds to run!
+print STDERR "# One minute left.\n";
 ok(prompt('Press <Enter> or wait 60 seconds. '), '');           # 60 seconds to run!
-
 
 exit;
